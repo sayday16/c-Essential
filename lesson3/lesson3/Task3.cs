6279a0e3eb7e57721e68741f060da5899be7902d
Kryshchyshyn Oleksii <oozoleksa@gmail.com>
2026-04-22 15:40:49 +0300

    Add missing code to Task3

--- lesson3/lesson3/Task3.cs
+++ lesson3/lesson3/Task3.cs
@@ -1,2 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace task3
+{
+    class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.WriteLine($"{value}");
+        }
+    }
+
+    class RedPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            base.Print(value);
+            Console.ResetColor();
+        }
+    }
+
+    class GreenPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            base.Print(value);
+            Console.ResetColor();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer[] printers = new Printer[3];
+
+            printers[0] = new Printer();
+            printers[1] = new RedPrinter();
+            printers[2] = (Printer)new GreenPrinter();
+
+            foreach (Printer p in printers)
+            {
+                p.Print("Work of printer");
+            }
+
+            Console.WriteLine();
+
+            Printer somePrinter = new RedPrinter();
+
+            if (somePrinter is RedPrinter)
+            {
+                RedPrinter myRedPrinter = (RedPrinter)somePrinter;
+                myRedPrinter.Print("Downcasting");
+            }
+        }
+    }
+}
