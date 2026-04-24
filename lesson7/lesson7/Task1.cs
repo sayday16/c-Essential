c5a02a2aa06abbb95a526c544df9633e05c6cf1a
Kryshchyshyn Oleksii <oozoleksa@gmail.com>
2026-04-24 19:26:03 +0300

    homework-7 structures and their varieties

---
+++ lesson7/lesson7/Task1.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace lesson7
+{
+    struct Train
+    {
+       public string Destination {  get; set; }
+       public int NumberOfTrain {  get; set; }
+       public DateTime Time { get; set; }
+
+       public  Train(string destination, int numberOfTrain, DateTime time)
+        {
+            Destination = destination;
+            NumberOfTrain = numberOfTrain;
+            Time = time;
+        }
+    }
+
+    class Program
+    {
+        static void Main1(string[] args)
+        {          
+            int N = 8;
+            Train[] t = new Train[N];
+            
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("enter destination: ");
+                string tempDest = Console.ReadLine();
+                Console.WriteLine("enter number of train: ");
+                int tempNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter date: ");
+                DateTime tempDate = DateTime.Parse(Console.ReadLine());
+                t[i] = new Train(tempDest, tempNum, tempDate);
+            }
+            
+            for(int i =0; i < N; i++)
+            {
+                for(int j = 0; j < N - 1 - i; j++)
+                {
+                    if (t[j].NumberOfTrain > t[j + 1].NumberOfTrain)
+                    {
+                        Train temp = t[j];
+                        t[j] = t[j + 1];
+                        t[j + 1] = temp;
+                    }
+                }
+            }
+            
+
+            Console.WriteLine("enter number of train or 0 to stop: ");
+            int searchNum = int.Parse(Console.ReadLine());
+            while (searchNum != 0) 
+            {
+                bool isFound = false;
+                for (int i = 0; i < N; i++) {
+                    if (t[i].NumberOfTrain == searchNum)
+                    {
+                        Console.WriteLine($"dest: {t[i].Destination}, time: {t[i].Time}");
+                        isFound = true;
+                    }                   
+                }
+                if (!isFound)
+                {
+                    Console.WriteLine("dont found");
+                }
+                Console.WriteLine("enter number of train or 0 to stop: ");
+                searchNum = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+    
+}
---
+++ lesson7/lesson7/Task2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson7
+{
+    class MyClass
+    {
+        public string change {  get; set; }
+    }
+
+    struct MyStruct
+    {
+        public string change { get; set; }
+    }
+
+    class Program2
+    {
+        static void Main()
+        {
+
+            static void ClassTaker(MyClass myClass)
+            {
+                myClass.change = "changed";
+            }
+
+            static void StructTaker(MyStruct myStruct)
+            {
+                myStruct.change = "changed";
+            }
+
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+            myClass.change = "not changed";
+            myStruct.change = "not changed";
+
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+
+            Console.WriteLine($"Клас: {myClass.change}");
+            Console.WriteLine($"Структура: {myStruct.change}");
+
+        }
+    }
+}
